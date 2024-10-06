ecaa17d11184b9c760cdc41cee6e85812dbf239d
Nihahaha <181787004+stardust-blue@users.noreply.github.com>
2024-10-06 15:11:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
 import sys
+import random
 
 
-def create_board():
+def get_mode() -> bool:
+    try:
+        mode = str(input("(M)anual or (A)utomatic?: ")).lower()
+        while mode != "m" and mode != "a":
+            print("That's not a valid mode!")
+            mode = str(input("(M)anual or (A)utomatic?: ")).lower()
+    except KeyboardInterrupt:
+        print("\nExiting...")
+        sys.exit()
+    return True if mode == "m" else False
+
+
+def get_names() -> dict[int:str]:
+    player_name = {}
+    try:
+        player_name[1] = input("Name of player (X): ").capitalize()
+        player_name[-1] = input("Name of player (O): ").capitalize()
+    except KeyboardInterrupt:
+        print("\nExiting...")
+        sys.exit()
+    return player_name
+
+
+def create_board() -> list[list[any]]:
     return [[None, None, None], [None, None, None], [None, None, None]]
 
 
-def render(board: list[list[None]]):
-    print("  0 1 2")
+def render(board: list[list[any]]):
+    print("  " + " ".join("012"))
     for row in range(3):
         print(
             str(row)
@@ -15,31 +39,39 @@ def render(board: list[list[None]]):
         )
 
 
-def get_move():
+def get_move(player_name) -> tuple[int, int]:
     try:
-        x = int(input("Your move (x): "))
-        y = int(input("Your move (y): "))
+        x = int(
+            input(
+                "X coordinate for player {cur_player}: ".format(cur_player=player_name)
+            )
+        )
+        y = int(
+            input(
+                "Y coordinate for player {cur_player}: ".format(cur_player=player_name)
+            )
+        )
     except KeyboardInterrupt:
         print("\nExiting...")
         sys.exit()
-    coords = (x, y)
-    return coords
+    return (x, y)
 
 
-def is_valid_move(board: list[list[any]], coords: tuple[int, int]):
-    if board[coords[1]][coords[0]] is not None:
+def is_valid_move(board: list[list[any]], coords: tuple[int, int]) -> bool:
+    if board[coords[0]][coords[1]] is not None:
         return False
     else:
         return True
 
 
-def make_move(old_board: list[list[any]], coords: tuple[int, int], player):
-    new_board = old_board
-    new_board[coords[1]][coords[0]] = player
-    return new_board
+def make_move(
+    board: list[list[any]], coords: tuple[int, int], player
+) -> list[list[any]]:
+    board[coords[0]][coords[1]] = player
+    return board
 
 
-def get_winner(board: list[list[any]]):
+def get_winner(board: list[list[any]]) -> int:
     lines = set()
 
     for row in range(3):
@@ -52,11 +84,15 @@ def get_winner(board: list[list[any]]):
 
     for line in lines:
         if len(line) == 1:
-            return next(iter(line))
+            winner = next(iter(line))
+            if winner == "X":
+                return 1
+            elif winner == "O":
+                return -1
     return None
 
 
-def check_draw(board: list[list[any]]):
+def check_draw(board: list[list[any]]) -> bool:
     flag = True
 
     for row in range(3):
@@ -68,27 +104,91 @@ def check_draw(board: list[list[any]]):
     return flag
 
 
-def main():
-    board = create_board()
+def random_move(board: list[list[any]]) -> tuple[int, int]:
+    x, y = random.randint(0, 2), random.randint(0, 2)
+    while board[x][y] is not None:
+        x, y = random.randint(0, 2), random.randint(0, 2)
+    return (x, y)
 
-    player = {1: "X", -1: "O"}
-    move_coords = None
+
+def manual_mode():
+    board = create_board()
+    player_symbol = {1: "X", -1: "O"}
+    player_name = get_names()
     turn = 1
+
     while True:
-        move_coords = get_move()
+        render(board)
+
+        move_coords = get_move(player_name[turn])
         if is_valid_move(board, move_coords):
-            board = make_move(board, move_coords, player[turn])
+            board = make_move(board, move_coords, player_symbol[turn])
             render(board)
             turn *= -1
+
             winner = get_winner(board)
             if winner is not None:
-                print(winner + " wins!")
+                print("Congratulations! " + player_name[winner] + " wins!")
                 break
             elif check_draw(board):
                 print("Draw!")
                 break
         else:
             print("Illegal move")
+
+
+def automatic_mode():
+    board = create_board()
+    player_symbol = {1: "X", -1: "O"}
+    player_name = str(input("Name of player: ")).capitalize()
+
+    render(board)
+    while True:
+        move_coords = get_move(player_name)
+        if is_valid_move(board, move_coords):
+            board = make_move(board, move_coords, player_symbol[1])
+            render(board)
+
+            winner = get_winner(board)
+            if winner == 1:
+                print("Congratulations! " + player_name + " wins!")
+                break
+            elif winner == -1:
+                print(
+                    "{current_name} lost! How unfortunate!".format(
+                        current_name=player_name
+                    )
+                )
+                break
+            elif check_draw(board):
+                print("Draw!")
+                break
+
+        board = make_move(board, random_move(board), player_symbol[-1])
+        print("AI has moved")
+
+        render(board)
+        winner = get_winner(board)
+        if winner == 1:
+            print("Congratulations! " + player_name + " wins!")
+            break
+        elif winner == -1:
+            print(
+                "{current_name} lost! How unfortunate!".format(current_name=player_name)
+            )
+            break
+        elif check_draw(board):
+            print("Draw!")
+            break
+
+
+def main():
+    mode = get_mode()
+
+    if mode:
+        manual_mode()
+    else:
+        automatic_mode()
 
 
 if __name__ == "__main__":
