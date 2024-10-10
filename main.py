a24768d562a33f9a745c78f59fd698ed5ced57e5
Nihahaha <181787004+stardust-blue@users.noreply.github.com>
2024-10-09 21:31:54 -0400

    Refactor main.py

--- main.py
+++ main.py
@@ -3,19 +3,19 @@ import random
 
 
 def get_mode() -> bool:
-    try:
-        mode = str(input("(M)anual or (A)utomatic?: ")).lower()
-        while mode != "m" and mode != "a":
-            print("That's not a valid mode!")
-            mode = str(input("(M)anual or (A)utomatic?: ")).lower()
-    except KeyboardInterrupt:
-        print("\nExiting...")
-        sys.exit()
-    return True if mode == "m" else False
+    while True:
+        try:
+            mode = input("(M)anual or (A)utomatic?: ").lower()
+            if mode != "m" and mode != "a":
+                raise ValueError("Please enter 'M' or 'A'.")
+            return True if mode == "m" else False
+        except KeyboardInterrupt:
+            print("\nExiting...")
+            sys.exit()
 
 
 def get_names() -> dict[int:str]:
-    player_name = {}
+    player_name = dict()
     try:
         player_name[1] = input("Name of player (X): ").capitalize()
         player_name[-1] = input("Name of player (O): ").capitalize()
@@ -40,28 +40,23 @@ def render(board: list[list[any]]):
 
 
 def get_move(player_name) -> tuple[int, int]:
-    try:
-        x = int(
-            input(
-                "X coordinate for player {cur_player}: ".format(cur_player=player_name)
-            )
-        )
-        y = int(
-            input(
-                "Y coordinate for player {cur_player}: ".format(cur_player=player_name)
-            )
-        )
-    except KeyboardInterrupt:
-        print("\nExiting...")
-        sys.exit()
-    return (x, y)
+    while True:
+        try:
+            x = int(input(f"X coordinate for player {player_name}: "))
+            y = int(input(f"Y coordinate for player {player_name}: "))
+
+            if x < 0 or y < 0:
+                raise ValueError("Please enter non-negative intergers.")
+            return (x, y)
+        except ValueError as error:
+            print(f"Invalid input: {error}. Please enter non-negative integers.")
+        except KeyboardInterrupt:
+            print("\nExiting...")
+            sys.exit()
 
 
 def is_valid_move(board: list[list[any]], coords: tuple[int, int]) -> bool:
-    if board[coords[0]][coords[1]] is not None:
-        return False
-    else:
-        return True
+    return False if board[coords[0]][coords[1]] is not None else True
 
 
 def make_move(
@@ -94,13 +89,11 @@ def get_winner(board: list[list[any]]) -> int:
 
 def check_draw(board: list[list[any]]) -> bool:
     flag = True
-
     for row in range(3):
         for col in range(3):
             if board[row][col] is None:
                 flag = False
                 break
-
     return flag
 
 
@@ -119,7 +112,6 @@ def manual_mode():
 
     while True:
         render(board)
-
         move_coords = get_move(player_name[turn])
         if is_valid_move(board, move_coords):
             board = make_move(board, move_coords, player_symbol[turn])
@@ -128,19 +120,23 @@ def manual_mode():
 
             winner = get_winner(board)
             if winner is not None:
-                print("Congratulations! " + player_name[winner] + " wins!")
+                print(f"Congratulations! {player_name[winner]} wins!")
                 break
             elif check_draw(board):
-                print("Draw!")
+                print("It's a draw!")
                 break
         else:
-            print("Illegal move")
+            print("Illegal move. Please try again.")
 
 
 def automatic_mode():
     board = create_board()
     player_symbol = {1: "X", -1: "O"}
-    player_name = str(input("Name of player: ")).capitalize()
+    try:
+        player_name = input("Name of player: ").capitalize()
+    except KeyboardInterrupt:
+        print("\nExitting...")
+        sys.exit()
 
     render(board)
     while True:
@@ -151,34 +147,30 @@ def automatic_mode():
 
             winner = get_winner(board)
             if winner == 1:
-                print("Congratulations! " + player_name + " wins!")
+                print(f"Congratulations! {player_name} wins!")
                 break
             elif winner == -1:
-                print(
-                    "{current_name} lost! How unfortunate!".format(
-                        current_name=player_name
-                    )
-                )
+                print(f"{player_name} lost! How unfortunate!")
                 break
             elif check_draw(board):
-                print("Draw!")
+                print("It's a draw!")
                 break
+        else:
+            print("Invalid move. Please try again.")
 
         board = make_move(board, random_move(board), player_symbol[-1])
-        print("AI has moved")
+        print("AI has moved.")
 
         render(board)
         winner = get_winner(board)
         if winner == 1:
-            print("Congratulations! " + player_name + " wins!")
+            print(f"Congratulations! {player_name} wins!")
             break
         elif winner == -1:
-            print(
-                "{current_name} lost! How unfortunate!".format(current_name=player_name)
-            )
+            print(f"{player_name} lost! How unfortunate!")
             break
         elif check_draw(board):
-            print("Draw!")
+            print("It's a draw!")
             break
 
 
